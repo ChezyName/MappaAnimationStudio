e6fcbb5d349fb65f9042f89f6ed0da03e0f74653
ChezyName <mineboy2468@gmail.com>
2024-06-19 22:35:43 -0500

    Added a New Employee/Worker Model

---
+++ Assets/Scripts/RandomEmployeeColoring.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEmployeeColoring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer)
+        {
+            // 3 = eyes
+            // 2 = hair
+            // 0 = shirt
+
+            renderer.materials[0].color = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+            renderer.materials[3].color = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+            renderer.materials[2].color = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+        }
+    }
+}
